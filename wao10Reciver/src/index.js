68a1b75fd05ec7291417940e5bac8c19cc8b21cf
Useless-4every1 <104831784+UselessDross@users.noreply.github.com>
2024-03-02 15:30:01 +0100

    did some cleaning to make the index.js files look less overwelming

--- wao10Reciver/src/index.js
+++ wao10Reciver/src/index.js
@@ -1,45 +1,21 @@
-import express from "express";
-const app = express();
-app.use(express.json());
-
 import { consumeMessage } from "./receive.js";
-
+import express            from "express";
+import amqp               from "amqplib";
 const PORT = process.env.PORT || 4002;
-
-import  amqp from "amqplib";
-var channel, connection;
-
-
-connectQueue() // call connectQueue function
-async function connectQueue() {
-    try {
-        connection = await amqp.connect("amqp://localhost:5672");
-        channel    = await connection.createChannel()
-        
-        // connect to 'test-queue', create one if doesnot exist already
-        await channel.assertQueue("test-queue")
-        
+const app  = express();
+      app.use(express.json());
 
       
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-
-
-app.get("/recive-msg", (req, res) => {
-
-    consumeMessage(channel, connection)
-    
-})
-
-
-
-
-
-
-
-
-
+var channel, connection;
+connectQueue() // call connectQueue function
+async function connectQueue() {
+                               try {
+                                    connection = await amqp.connect("amqp://localhost:5672");
+                                    channel    = await connection.createChannel()
+                                                 await channel.assertQueue("test-queue")
+                                    } catch (error) { console.log(error) }
+                               }
+app.get("/recive-msg", (req, res) => { 
+                                     consumeMessage(channel, connection) 
+                                     })
 app.listen(PORT, () => console.log("Server running at port " + PORT));
--- wao10Sender/src/index.js
+++ wao10Sender/src/index.js
@@ -1,44 +1,30 @@
-import express from "express";
-const app = express();
-const PORT = process.env.PORT || 4001;
-
-app.use(express.json());
-
 import { sendData } from "./send.js";
+import express      from "express";
+import amqp         from "amqplib";
+const PORT = process.env.PORT || 4001;
+const app  = express();
+      app.use(express.json());
 
-import  amqp from "amqplib";
+
 var channel, connection;
-
-
 connectQueue() // call connectQueue function
 async function connectQueue() {
-    try {
-
-        connection = await amqp.connect("amqp://localhost:5672");
-        channel    = await connection.createChannel()
-        
-        // connect to 'test-queue', create one if doesnot exist already
-        await channel.assertQueue("test-queue")
-        
-    } catch (error) {
-        console.log(error)
-    }
-}
+                                try {
+                                     connection = await amqp.connect("amqp://localhost:5672");
+                                     channel    = await connection.createChannel()
+                                                  await channel.assertQueue("test-queue")
+                                     } catch (error) { console.log(error) }
+                              }
 
 
 
 app.get("/send-msg", (req, res) => {
-    const data = {
-        title: "Six of Crows",
-        author: "Leigh Burdugo"
-    }
-
-    sendData(data,channel,connection);
-
-    console.log("A message is sent to queue")
-    res.send("Message Sent");
-    
-})
-
-
+                                   const data = {
+                                                 title: "Six of Crows",
+                                                 author: "Leigh Burdugo"
+                                                 }
+                                   sendData(data,channel,connection);
+                                   console.log("A message is sent to queue")
+                                   res.send("Message Sent"); 
+                                   })
 app.listen(PORT, () => console.log("Server running at port " + PORT));
